b2b30406a6e6cfc4f4f9b739a4d5239cb44d1ad8
kels184 <kelsey.webber@my.jcu.edu.au>
2023-03-25 13:43:26 +1000

    began a quarto file for analysing recruitment

---
+++ R/analysis_recruitment.R
@@ -0,0 +1,29 @@
+## ---- loadFunctions
+source('../R/functions.R') # includes setup fns, packages, brms DHARMA fns and SUYR Prior_Posterior fn
+## ----end
+
+# Fish Recruitment in Macroalgal Patch Experiment **************************************************
+
+
+## ---- recruitment readData
+
+fishdata <- read_csv(paste0(DATA_PATH,
+                        "primary/fish1.csv"),
+                 trim_ws=TRUE)
+algaedata <- read_csv(paste0(DATA_PATH,
+                        "primary/algae.csv"),
+                 trim_ws=TRUE)
+
+## ---- end
+
+## ---- recruitment glimpse
+
+fishdata %>% glimpse()
+algaedata %>% glimpse()
+
+## ---- end
+
+## ---- recruitment report table
+fishdata %>% report::report_table() 
+algaedata %>% report::report_table() 
+## ----end
