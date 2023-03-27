aa97e16d14cf1d8403625de9ba65656ea09fb2d2
kels184 <kelsey.webber@my.jcu.edu.au>
2023-03-27 11:40:33 +1000

    created algae eda plots

--- R/analysis_recruitment.R
+++ R/analysis_recruitment.R
@@ -27,3 +27,51 @@ algaedata %>% glimpse()
 fishdata %>% report::report_table() 
 algaedata %>% report::report_table() 
 ## ----end
+
+## ----factorise and clean
+fishdata <- fishdata %>% 
+  mutate(Date = factor(Date), #note this will probs need to be ordered later
+         Treatment = factor(Treatment),
+         Replicate = factor(Replicate),
+         Family = factor(Family),
+         Species = factor(Species)) %>% 
+  select(-c(Other, Replacement, `Patch note`, Transparency, `ID note`, `Behaviour note`))
+glimpse(fishdata)
+
+algaedata <- algaedata %>% 
+  mutate(Treatment = factor(Treatment),
+         Replicate = factor(Replicate),
+         Thallus = factor(Thallus)) %>% 
+  rename(Length = `Length (cm)`,
+         Weight = `Weight (g)`)
+
+glimpse(algaedata)
+## ----end
+
+
+## ---- recruitment algae EDA
+algae.sum <- algaedata %>% 
+  group_by(Treatment) %>% 
+  summarise(mean.len = mean(Length),
+            n.thalli = length(Replicate),
+            SE.len = sd(Length)/sqrt(length(Replicate)),
+            mean.wt = mean(Weight),
+            SE.wt = sd(Weight)/sqrt(length(Replicate)) 
+            )
+algae.sum
+g.len <- ggplot(algaedata) + aes(y = Length, x = Treatment) + 
+  geom_point(alpha = 0.1) +
+  geom_violin(fill = NA) + 
+  theme_bw()
+
+g.len  
+
+g.wt <- ggplot(algaedata) + aes(y = Weight, x = Treatment) +
+  geom_point(alpha = 0.1) + 
+  geom_violin(fill = NA) +
+  labs(y = "Length (cm)") +
+  theme_bw(text = element_text(family = "Calibri", size = 8, color = "black"),
+           axis.text = element_text(size = 10),
+           axis.title = element_text(size = 12)
+           )
+## ----end
